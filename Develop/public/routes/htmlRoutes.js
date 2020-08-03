c17d22add91c5f4a638704e5b9a4dc4bd4d6f63c
Natalia Teaca <lilikoy@sbcglobal.net>
2020-08-02 22:23:48 -0700

    fixed note error

--- Develop/public/routes/htmlRoutes.js
+++ Develop/public/routes/htmlRoutes.js
@@ -1,12 +1,12 @@
-var path = require("path");
-var router = require("express").Router();
+// var path = require("path");
+// var router = require("express").Router();
 
-router.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+// router.get("/notes", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+// });
 
-router.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-})
+// router.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// })
 
-module.exports = router;
+// module.exports = router;
